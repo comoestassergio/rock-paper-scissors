b6857633ccac6e3f6a3187328c140deac6c46160
sergio <dirtycrew666@gmail.com>
2022-05-25 13:43:02 +0500

    Finish engine, UI still slightly off

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const paper = document.querySelector(".paper")
 const scissors = document.querySelector(".scissors")
 const result = document.querySelector(".result")
 const buttons = document.querySelectorAll("button")
+const win = document.querySelector(".win")
+const lose = document.querySelector(".lose")
+const btnContainer = document.querySelector(".btn-container")
+const resultSection = document.querySelector(".result-section")
+const body = document.querySelector("body")
+
+
+let wins = 0
+let loses = 0
 
 buttons.forEach(button => {
     button.addEventListener("transitionend", function(){
@@ -49,16 +58,34 @@ function playGame(input) {
         result.textContent = "It's a draw!!!"
     } else if (userChoice === "Rock" && compChoice === "Scissors") {
         result.textContent = `${userChoice} beats ${compChoice}!`
+        incrementWins()
+        updateWins()
+        checkScore(wins,loses)
     } else if (userChoice === "Rock" && compChoice === "Paper") {
         result.textContent =`${compChoice} beats ${userChoice}!`
+        incrementLoses()
+        updateLoses()
+        checkScore(wins,loses)
     } else if (userChoice === "Paper" && compChoice === "Scissors") {
         result.textContent =`${compChoice} beats ${userChoice}!`
+        incrementLoses()
+        updateLoses()
+        checkScore(wins,loses)
     } else if (userChoice === "Paper" && compChoice === "Rock") {
         result.textContent = `${userChoice} beats ${compChoice}!`
+        incrementWins()
+        updateWins()
+        checkScore(wins,loses)
     } else if (userChoice === "Scissors" && compChoice === "Paper") {
         result.textContent = `${userChoice} beats ${compChoice}!`
+        incrementWins()
+        updateWins()
+        checkScore(wins,loses)
     } else if (userChoice === "Scissors" && compChoice === "Rock") {
         result.textContent = `${compChoice} beats ${userChoice}!`
+        incrementLoses()
+        updateLoses()
+        checkScore(wins,loses)
     }
 }
 
@@ -66,11 +93,77 @@ function playGame(input) {
 function checkScore(score1, score2){
     if (score1 === 5 && score2 === 5){
         console.log("It's a draw!")
+        gameOver(1)
     }
     if (score1 === 5){
         console.log("You win!")
+        gameOver(2)
     }
     if (score2 === 5){
         console.log("You lose!")
+        gameOver(3)
+    }
+}
+
+function incrementWins(){
+    wins++
+}
+
+function incrementLoses(){
+    loses++
+}
+
+function updateWins(){
+    win.textContent = `Win: ${wins}`
+}
+
+function updateLoses(){
+    lose.textContent = `Lose: ${loses}`
+}
+
+
+function gameOver(option) {
+    resultSection.style.display = "none"
+    btnContainer.style.display = "none"
+    const para = document.createElement("p")
+    const tryAgainBtn = document.createElement("button")
+
+    if (option === 1){
+        para.style.cssText = "color: yellow; font-size: 6rem; font-weight: bold;"
+        para.textContent = "It's a draw!"
+    }
+    if (option === 2){
+        para.style.cssText = "color: green; font-size: 6rem; font-weight: bold"
+        para.textContent = "You win!"
+    }
+    if (option === 3){
+        para.style.cssText = "color: red; font-size: 6rem; font-weight: bold"
+        para.textContent = "You lose!"
+    }
+
+    tryAgainBtn.textContent = "Try again"
+    body.appendChild(para)
+    body.appendChild(tryAgainBtn)
+
+    tryAgainBtn.addEventListener("click", function(){
+        tryAgainBtn.classList.add("active")
+        reset()
+    })
+
+    function reset() {
+        wins = 0
+        loses = 0
+    
+        body.removeChild(para)
+        body.removeChild(tryAgainBtn)
+    
+        resultSection.style.display = "flex"
+        btnContainer.style.display = "block"
+        result.textContent = ""
+        updateLoses()
+        updateWins()
+        buttons.forEach(btn => {
+            btn.classList.remove("active")
+        })
     }
 }
